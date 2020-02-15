eee2ef6e8d97de73925c026d5629aacb35acdaa9
Ahmed-Th <60985434+Ahmed-Th@users.noreply.github.com>
2020-02-15 17:52:29 +0200

    Add files via upload

---
+++ react/src/Components/Navbar/Navbar.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  bar:{
+    backgroundColor:"#F0F1F3",
+  },
+  menuButton: {
+    display:'none',
+    [theme.breakpoints.down('xs')]: {
+    display:'block',
+    color:'black'
+    },
+    
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+      marginLeft:theme.spacing(2),
+      marginRight:theme.spacing(10),
+      [theme.breakpoints.down('sm')]: {
+        marginRight:theme.spacing(2),
+        marginLeft:theme.spacing(0),
+      },
+      fontSize:'1.8rem',
+      fontWeight:'bold',
+    },
+    color:'black',
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: 'white',
+    marginRight:theme.spacing(1),
+    borderRadius:'10px',
+    
+    '&:hover': {
+     
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 'auto',
+    },
+    [theme.breakpoints.down('sm')]: {
+      width: '40%',
+    },
+    [theme.breakpoints.down('xs')]: {
+      width: '100%',
+    },
+  },
+  
+  inputRoot: {
+    color: 'grey',
+    
+  },
+  inputInput: {
+    padding: theme.spacing(1, 19, 1, 2),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+     
+      '&:focus': {
+        
+      },
+    },
+  },
+  btn: {
+    backgroundImage:"linear-gradient(to right, #28b485  , #7ed56f)",
+    color:'white',
+    borderRadius:'1rem',
+    fontSize:'1rem',
+    padding: theme.spacing(1,2),
+    [theme.breakpoints.down('xs')]: {
+        display:'none'
+    },
+   
+    '&:hover': {
+      
+    },
+
+  },
+  login: {
+    flexGrow:1,
+    fontWeight:'bolder',
+    color:'black',
+    fontSize:'1.2rem',
+    [theme.breakpoints.down('xs')]: {
+      display:'none'
+  },
+    
+  }
+}));
+
+export default function Navbar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.bar}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+           SelfibediA
+          </Typography>
+          <div className={classes.search}>
+            <InputBase
+              placeholder="Search by name,education,etc... "
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+          
+          <Button className={classes.btn}>LOOk UP</Button>
+          <Button className={classes.login}>Login</Button>
+
+          
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
